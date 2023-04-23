4bb2f6afdca573ffbcfc4b37c6d5a81accb6605b
Bobtastisch <e.jerber@web.de>
2023-04-23 19:21:43 +0200

    ReadTxtFile for reading the id and pw of twitch acc

--- TwitchRaid/Program.cs
+++ TwitchRaid/Program.cs
@@ -2,9 +2,12 @@
 {
     internal class Program
     {
+        private String init = "G:\\TwitchRaid\\TwitchRaid\\TwitchRaid\\Init.txt"
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! ");
+            
+            
         }
     }
 }
---
+++ TwitchRaid/ReadTxtFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchRaid
+{
+    public class ReadTxtFile
+    {
+        public static string ReadFile(string filePath)
+        {
+            string content = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                // Handle any errors that might occur
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            return content;
+        }
+    }
+}
